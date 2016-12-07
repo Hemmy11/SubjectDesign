ac2c820eb4cfc98e75ba2ba8247184a4ca2c7a5d
ASUS <ASUS@DESKTOP-LT4N694>
2016-12-08 01:37:44 +0800

    add frame details

---
+++ src/com/fido/table/ComButtonEditor.java
@@ -0,0 +1,95 @@
+package com.fido.table;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.AbstractCellEditor;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.TableCellEditor;
+
+public class ComButtonEditor extends AbstractCellEditor implements
+TableCellEditor {
+
+/**
+* serialVersionUID
+*/
+private static final long serialVersionUID = -6546334664166791132L;
+
+private JPanel panel;
+
+private JButton button;
+
+public ComButtonEditor() {
+
+initButton();
+
+initPanel();
+
+panel.add(this.button, BorderLayout.CENTER);
+}
+
+private void initButton() {
+button = new JButton();
+
+button.addActionListener(new ActionListener() {
+    public void actionPerformed(ActionEvent e) {  //按下查看按钮出照片
+        JFrame jf=new JFrame();
+        JPanel panel=new JPanel();
+        panel.setSize(200,200);
+        jf.getContentPane().add(panel);
+        JLabel photoLabel=new JLabel();
+        ImageIcon image=new ImageIcon("D://PPT素材//蓝色.jpg");
+        jf.setBounds(600,300,200,200);
+        image.setImage(image.getImage().getScaledInstance(jf.getWidth(),jf.getHeight(),Image.SCALE_DEFAULT)); 
+        photoLabel.setIcon(image);
+        panel.add(photoLabel);
+        jf.setVisible(true);
+        jf.setResizable(false);
+
+    }
+});
+
+}
+
+private void initPanel() {
+panel = new JPanel();
+
+panel.setLayout(new BorderLayout());
+}
+
+@Override
+public Component getTableCellEditorComponent(JTable table, Object value,
+    boolean isSelected, int row, int column) {
+
+button.setText("查看");
+button.setFont(new Font("微软雅黑", Font.BOLD, 12));
+button.setBackground(Color.LIGHT_GRAY);
+button.addActionListener(new ActionListener(){ //查看对手的照片
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		
+	}
+});
+
+return panel;
+}
+
+@Override
+public Object getCellEditorValue() {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+}
---
+++ src/com/fido/table/ComButtonRenderer.java
@@ -0,0 +1,48 @@
+package com.fido.table;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+public class ComButtonRenderer implements TableCellRenderer {
+    private JPanel panel;
+
+    private JButton button;
+    
+
+    public ComButtonRenderer() {
+        initButton();
+
+        initPanel();
+
+        panel.add(button, BorderLayout.CENTER);
+    }
+
+    private void initButton() {
+        button = new JButton();
+
+    }
+
+    private void initPanel() {
+        panel = new JPanel();
+
+        panel.setLayout(new BorderLayout());
+    }
+
+    public Component getTableCellRendererComponent(JTable table, Object value,
+            boolean isSelected, boolean hasFocus, int row, int column) {
+        
+        button.setText("²é¿´");
+        button.setFont(new Font("Î¢ÈíÑÅºÚ", Font.BOLD, 12));
+        button.setBackground(Color.LIGHT_GRAY);
+
+        return panel;
+    }
+
+}
